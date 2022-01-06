726ea0a13ce8d36fbc2a2dc9dead9b91dc6cfe3e
azanane <anas.zanane1s5@gmail.com>
2022-01-06 13:37:27 +0100

    ajout

---
+++ FDFCORREC/includes/fdf.h
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azanane <azanane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/24 11:14:32 by anaszanane        #+#    #+#             */
+/*   Updated: 2022/01/06 11:28:39 by azanane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <math.h>
+# include "mlx.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdlib.h>
+# include "get_next_line.h"
+# include "utils.h"
+
+typedef struct s_v
+{
+	int		j;
+	int		n;
+	int		d;
+	int		k;
+	int		m;
+	int		x;
+	int		**tab;
+	int		fd;
+}	t_v;
+
+typedef struct s_algo
+{
+	int		x;
+	int		y;
+	int		dy;
+	int		dy1;
+	int		dx;
+	int		dx1;
+	int		m;
+	double	c;
+	char	*s;
+	char	**file;
+	int		**col;
+	int		col1;
+	int		col2;
+	double	col3;
+	void	*ptr;
+	void	*win;
+	void	*img;
+}	t_algo;
+
+int		main(int ac, char **av);
+void	ft_parsing(int i, t_algo *a, char *av);
+void	ft_wireframe(int **tab, int ymax, int xmax, t_algo	*a);
+char	**ft_freee(char	**tab);
+void	ft_put_col(t_algo *a, int xmax, int ymax);
+int		ft_free_int(int **tab, int max);
+
+#endif
---
+++ FDFCORREC/sources/utils/ft_atoi_base.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azanane <azanane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 17:19:29 by azanane           #+#    #+#             */
+/*   Updated: 2022/01/06 11:25:50 by azanane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+#include "get_next_line.h"
+
+int	ft_b(char *str, char *base)
+{
+	int	nb;
+	int	i;
+	int	j;
+	int	ct;
+
+	nb = 0;
+	i = -1;
+	ct = 1;
+	if (str[++i] == '-')
+		ct *= -1;
+	else if (str[i] == '+')
+		i++;
+	while (str[i - 1] != 'x' && str[i - 1] != 'X')
+		i++;
+	while (str[i] != '\n' && str[i] != 0 && str[i] != ' ')
+	{
+		j = 0;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = ft_toupper(str[i]);
+		while (base[j] && base[j] != str[i])
+			j++;
+		nb = nb * ft_strlen(base) + j;
+		i++;
+	}
+	return (nb);
+}
