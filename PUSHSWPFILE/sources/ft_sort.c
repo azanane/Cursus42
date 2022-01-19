6c83a4102cd85e3e4a15875b1252b069aaa1beec
azanane <anas.zanane1s5@gmail.com>
2022-01-19 16:56:32 +0100

    End of push_swap 01/19/2022

--- PUSHSWPFILE/sources/ft_sort.c
+++ PUSHSWPFILE/sources/ft_sort.c
@@ -6,11 +6,50 @@
 /*   By: azanane <azanane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 15:38:29 by azanane           #+#    #+#             */
-/*   Updated: 2022/01/14 16:43:19 by azanane          ###   ########.fr       */
+/*   Updated: 2022/01/19 07:57:54 by azanane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swp.h"
+
+int	ft_place(t_v *v, int nb)
+{
+	int	i;
+
+	i = 0;
+	while (v->tab[0][i] != nb)
+		i++;
+	if (i <= 2)
+		return (0);
+	return (1);
+}
+
+void	ft_sort_3(t_v *v)
+{
+	if (ft_place(v, 0) == 0)
+	{
+		while (v->tab[0][0] != 0)
+			ft_operation(v, "ra");
+	}
+	else
+	{
+		while (v->tab[0][0] != 0)
+			ft_operation(v, "rra");
+	}
+	ft_operation(v, "pb");
+	v->ct2 = 1;
+	while (v->tab[0][0] != 1)
+		ft_operation(v, "ra");
+	ft_operation(v, "pb");
+	v->ct2 = 2;
+	while (v->tab[0][2] != 4)
+		ft_operation(v, "ra");
+	if (v->tab[0][0] > v->tab[0][1])
+		ft_operation(v, "sa");
+	ft_operation(v, "pa");
+	v->ct2 = 1;
+	ft_operation(v, "pa");
+}
 
 void	ft_sort_2(t_v *v)
 {
@@ -41,7 +80,7 @@ void	ft_sort(t_v *v)
 		ft_operation(v, "sa");
 	else if (v->d == 3)
 		ft_sort_2(v);
-	else
+	else if (v->d == 4)
 	{
 		while (v->tab[0][0] != 0)
 			ft_operation(v, "ra");
@@ -53,4 +92,6 @@ void	ft_sort(t_v *v)
 		v->ct2 = 1;
 		ft_operation(v, "pa");
 	}
+	else if (v->d == 5)
+		ft_sort_3(v);
 }
